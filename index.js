d5c01eb446d86902e50083cebdd73996f17ad957
Diego <diego@Diegos-Laptop.local>
2023-05-27 15:28:52 -0500

    update: code modified for best performing

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
 
-(async () => {
-    const browser = await chromium.launch()
-    const page = await browser.newPage()
+const shops = [
+  {
+    vendor: "Microsoft",
+    url: "https://www.xbox.com/es-es/configure/8WJ714N3RBTL",
+    checkStock: async ({ page }) => {
+      await page.screenshot({ path: "example.png" });
+      const content = await page.textContent(
+        '[aria-label="Finalizar la compra del pack"]'
+      );
+      return  content.includes("Finalizar la compra") === true;
+    },
+  },
+];
 
-    await page.goto('https://www.xbox.com/es-es/configure/8WJ714N3RBTL')
-    await page.screenshot({ path: 'example.png' })
-    const content = await page.textContent('[aria-label="Finalizar la compra del pack"]')
-    console.log(content.includes('Finalizar la compra'))
-    await browser.close()
-})()
+ (async () => {
+  const browser = await chromium.launch();
+
+    for (const shop of shops) {
+        const {checkStock, vendor, url} = shop
+        const page = await browser.newPage();
+        await page.goto(url);
+        const hasStock = await checkStock({page})
+        console.log(`${vendor}: ${hasStock ? 'in stock!!!!' : 'out of stock 😢'}`)
+    }
+
+  await browser.close();
+})();
+
+//Un scrapper es una técnica que se utiliza para abstraer info/data de una webpage
